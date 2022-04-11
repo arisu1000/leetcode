2ac034a79a88c0094ec6ee0e65d4a84a7c2fa77c
WonChon Jung <arisu1000@gmail.com>
2022-04-11 20:50:56 +0900

    Add readme

--- mergetwosortedlist.py
+++ mergetwosortedlist.py
@@ -6,59 +6,58 @@ class ListNode:
         self.val = val
         self.next = next
 
-# class Solution:
-#     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+class Solution:
+    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
 
-#         result = ListNode()
-#         nodelist = []
+        result = ListNode()
+        nodelist = []
 
-#         if list1 == None:
-#             return list2
-#         elif list2 == None:
-#             return list1
+        if list1 == None:
+            return list2
+        elif list2 == None:
+            return list1
 
-#         while list1 != None:
-#             while list2 != None:
+        while list1 != None:
+            while list2 != None:
 
-#                 if list1 == None:
-#                     nodelist.append(list2)
-#                     list2 = list2.next
-#                 elif list1.val <= list2.val:
-#                     nodelist.append(list1)
-#                     list1 = list1.next
-#                 else:
-#                     nodelist.append(list2)
-#                     list2 = list2.next
+                if list1 == None:
+                    nodelist.append(list2)
+                    list2 = list2.next
+                elif list1.val <= list2.val:
+                    nodelist.append(list1)
+                    list1 = list1.next
+                else:
+                    nodelist.append(list2)
+                    list2 = list2.next
             
-#             if list1 != None:
-#                 nodelist.append(list1)
-#                 list1 = list1.next
+            if list1 != None:
+                nodelist.append(list1)
+                list1 = list1.next
 
-#         result = None
-#         for i in range(len(nodelist), 0, -1):
-#             node = nodelist.pop()
-#             ln = ListNode(node.val, result)
-#             result = ln
+        result = None
+        for i in range(len(nodelist), 0, -1):
+            node = nodelist.pop()
+            ln = ListNode(node.val, result)
+            result = ln
 
-#         return result
+        return result
 
 
-class Solution(object):
-    def mergeTwoLists(self, l1, l2):
-        # dummy = ListNode(0)  
-        # cur = dummy  
-        cur = ListNode(0)
-        while l1 and l2:  
-            if l1.val <= l2.val:  
-                cur.next = l1  
-                l1 = l1.next  
-            else:  
-                cur.next = l2  
-                l2 = l2.next  
-            cur = cur.next  
-        cur.next = l1 or l2  
+# class Solution(object):
+#     def mergeTwoLists(self, l1, l2):
+#         dummy = ListNode(0)  
+#         cur = dummy  
+#         while l1 and l2:  
+#             if l1.val <= l2.val:  
+#                 cur.next = l1  
+#                 l1 = l1.next  
+#             else:  
+#                 cur.next = l2  
+#                 l2 = l2.next  
+#             cur = cur.next  
+#         cur.next = l1 or l2  
   
-        return cur.next  
+#         return dummy.next  
 
 s = Solution()
 

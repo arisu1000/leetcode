7d08c5df92549e59a3be80e2490428b2433e0dd3
WonChon Jung <arisu1000@gmail.com>
2024-03-30 21:11:58 +0900

    Jump Game 2

--- 45JumpGame2.py
+++ 45JumpGame2.py
@@ -1,20 +1,31 @@
+#https://leetcode.com/problems/jump-game-ii/description
+
 from typing import List
 
-#문제 내용을 이해 못했음.
 class Solution:
     def jump(self, nums: List[int]) -> int:
-        n = len(nums)
-        current = 0
-        long = 0
-        count = 0
-        for i in range(n - 1):
-            long =  max(i + nums[i], long)
-            if i == current:
-                current = long
-                count += 1
+        length = len(nums)
+        
+        targetIndex = length - 1
+        moveIndex = targetIndex - 1
+        countStep = 0
+        tmpIndex = 10000000
+        while moveIndex >= 0 and tmpIndex > 0:
+            if moveIndex + nums[moveIndex] >= targetIndex:
+                tmpIndex = moveIndex
+            
+            moveIndex -= 1
+            
+            if moveIndex < 0:
+                targetIndex = tmpIndex
+                moveIndex = tmpIndex - 1
+                countStep += 1
 
-        return count
+        return countStep
+
 
 s = Solution()
 print(s.jump([2,3,1,1,4]))
+print(s.jump([2,3,0,1,4]))
 print(s.jump([0]))
+print(s.jump([3,2,1]))

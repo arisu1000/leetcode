66c6a899713ef6851f7a89ec40efa26702431a4b
WonChon Jung <arisu1000@gmail.com>
2024-04-29 20:28:02 +0900

    Merge Intervals

--- 56MergeIntervals.py
+++ 56MergeIntervals.py
@@ -1,26 +1,30 @@
+#https://leetcode.com/problems/merge-intervals
+
 from typing import List
 
 class Solution:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        ans = []
+
         intervals.sort()
-        result = []
-        n = len(intervals)
 
         start = intervals[0][0]
         end = intervals[0][1]
-        for i in range(n):
-            if end < intervals[i][0]:
-                result.append([start, end])
+        for i in range(1, len(intervals)):
+            if intervals[i][0] <= end:
+                if intervals[i][1] > end:
+                    end = intervals[i][1]
+            else:
+                ans.append([start, end])
                 start = intervals[i][0]
-            
-            if end < intervals[i][1]:
                 end = intervals[i][1]
 
-        result.append([start, end])
-        
-        return result
+        ans.append([start, end])
+
+        return ans
 
 
 s = Solution()
-print(s.merge([[1,3],[2,6],[8,10],[15,18]]))
-print(s.merge([[[1,4],[4,5]]]))
+print(s.merge([[1,4],[2,3]]), [[1,4]])
+print(s.merge([[1,3],[2,6],[8,10],[15,18]]), [[1,6],[8,10],[15,18]])
+print(s.merge([[1,4],[4,5]]), [[1,5]])

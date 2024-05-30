a25b1eec7246e44bcdfb867863ac60227de7cafd
WonChon Jung <arisu1000@gmail.com>
2024-05-30 20:25:23 +0900

    Sum Root to Leaf Numbers

---
+++ 129SumRootToLeafNumbers.py
@@ -0,0 +1,67 @@
+#https://leetcode.com/problems/sum-root-to-leaf-numbers
+
+
+from typing import Optional
+from collections import deque
+
+def insert_level_order(arr, root, i, n):
+    # Base case for recursion
+    if i < n:
+        if arr[i] is None:
+            return None
+
+        temp = TreeNode(arr[i])
+        root = temp
+
+        # insert left child
+        root.left = insert_level_order(arr, root.left, 2 * i + 1, n)
+
+        # insert right child
+        root.right = insert_level_order(arr, root.right, 2 * i + 2, n)
+    
+    return root
+
+def build_tree(arr):
+    n = len(arr)
+    if n == 0:
+        return None
+    return insert_level_order(arr, None, 0, n)
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def sumNumbers(self, root: Optional[TreeNode]) -> int:
+        if root is None:
+            return 0
+        
+        pathNumberStr = ""
+
+        result = []
+        def makePathNumber(current: Optional[TreeNode], pathNumberStr: str) -> str:
+            if current.left is None and current.right is None:
+                pathNumberStr += str(current.val)
+                result.append(pathNumberStr)
+
+            if current.left is not None:
+                makePathNumber(current.left, pathNumberStr + str(current.val))
+            
+            if current.right is not None:
+                makePathNumber(current.right, pathNumberStr + str(current.val))
+
+        makePathNumber(root, pathNumberStr)
+
+        sum = 0
+        for r in result:
+            sum += int(r)
+
+        return sum
+
+s = Solution()
+print(s.sumNumbers(build_tree([1,2,3])), 25)        
+print(s.sumNumbers(build_tree([4,9,0,5,1])), 1026)        

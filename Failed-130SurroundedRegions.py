adedb208a95e29a8ecead07367e1ed27ce64b233
WonChon Jung <arisu1000@gmail.com>
2024-07-08 21:32:30 +0900

    Solve by chatgpt

--- Failed-130SurroundedRegions.py
+++ Failed-130SurroundedRegions.py
@@ -4,50 +4,41 @@ from typing import List
 
 class Solution:
 
-    def isSurround(self, r, c) -> bool:
-
-        if (r < 0 or r >= len(self.board)) or (c < 0 or c >= len(self.board[0])):
-            return False
-
-        if self.board[r][c] == 'X':
-            return True
-
-        if (r,c) in self.ans:
-            return True
-        
-        if (r,c) in self.visted:
-            return False
-
-        self.visted.add((r,c))
-
-        isR1 = self.isSurround(r-1,c)
-        isR2 = self.isSurround(r+1,c)
-        isR3 = self.isSurround(r,c-1)
-        isR4 = self.isSurround(r,c+1)
-
-        if (isR1 and r > 0) and (isR2 and r <= len(self.board)) and (isR3 and c > 0) and (isR4 and c <= len(board[0])):
-            self.ans.add((r,c))
-            return True
-
-    visted = set()
-    ans = set()
-    board = []
-
     def solve(self, board: List[List[str]]) -> None:
-        self.visted = set()
-        self.board = board
-        self.ans = set()
 
-        for r in range(len(board)):
-            for c in range(len(board[0])):
-                if board[r][c] == 'O' and not (r == 0 or r == len(board) - 1 or c == 0 or c == len(board[0])-1):
-                    
-                    self.isSurround(r,c)
-                    self.visted.add((r,c))
+        if not board:
+            return
 
-        for a in self.ans:
-            board[a[0]][a[1]] = 'X'
+        m, n = len(board), len(board[0])
 
+        def dfs(i, j):
+            if i < 0 or i >= m or j < 0 or j >= n or board[i][j] != 'O':
+                return
+            board[i][j] = 'T'
+            dfs(i + 1, j)
+            dfs(i - 1, j)
+            dfs(i, j + 1)
+            dfs(i, j - 1)
+
+
+        for i in range(m):
+            if board[i][0] == 'O':
+                dfs(i, 0)
+            if board[i][n-1] == 'O':
+                dfs(i, n-1)
+
+        for j in range(n):
+            if board[0][j] == 'O':
+                dfs(0, j)
+            if board[m - 1][j] == 'O':
+                dfs(m-1, j)
+        
+        for i in range(m):
+            for j in range(n):
+                if board[i][j] == 'O':
+                    board[i][j] = 'X'
+                elif board[i][j] == 'T':
+                    board[i][j] = 'O'
 
 
 
@@ -55,12 +46,16 @@ s = Solution()
 
 
 
-
 board = [["X","X","X","X"],["X","O","O","X"],["X","X","O","X"],["X","O","X","X"]]
 s.solve(board)
 for b in board:
     print(b)
-print([["X","X","X","X"],["X","X","X","X"],["X","X","X","X"],["X","O","X","X"]])
+
+print('-------------------------------')
+ans = [["X","X","X","X"],["X","X","X","X"],["X","X","X","X"],["X","O","X","X"]]
+for a in ans:
+    print(a)
+print('-------------------------------')
 
 board = [["X","O","X"],
          ["X","O","X"],
@@ -69,13 +64,19 @@ s.solve(board)
 for b in board:
     print(b)
 
-print([["X","O","X"],["X","O","X"],["X","O","X"]])
-
+print('-------------------------------')
+ans = [["X","O","X"],["X","O","X"],["X","O","X"]]
+for a in ans:
+    print(a)
+print('-------------------------------')
 
 board = [["O","O","O"],["O","O","O"],["O","O","O"]]
 s.solve(board)
 for b in board:
     print(b)
 
-print([["O","O","O"],["O","O","O"],["O","O","O"]])
-
+print('-------------------------------')
+ans = [["O","O","O"],["O","O","O"],["O","O","O"]]
+for a in ans:
+    print(a)
+print('-------------------------------')

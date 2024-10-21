60c18597bc3f70b92195832570508239a066c53d
WonChon Jung <arisu1000@gmail.com>
2024-10-21 20:32:50 +0900

    Change to binary

--- 153FindMinimuminRotatedSortedArray.py
+++ 153FindMinimuminRotatedSortedArray.py
@@ -6,14 +6,18 @@ class Solution:
     def findMin(self, nums: List[int]) -> int:
         left, right = 0, len(nums) - 1
 
-        minVal = nums[0]
+        while left < right:
+            mid = (left + right) // 2
+            if nums[mid] > nums[right]:
+                left = mid +1
+            else:
+                right = mid
 
-        for n in nums:
-            minVal = min(minVal, n)        
-
-        return minVal
+        return nums[left]
 
 s = Solution()
+print(s.findMin([5,1,2,3,4]), 1)
+print(s.findMin([2,1]), 1)
 print(s.findMin([3,4,5,1,2]), 1)
 print(s.findMin([4,5,6,7,0,1,2]), 0)
 print(s.findMin([11,13,15,17]), 11)
